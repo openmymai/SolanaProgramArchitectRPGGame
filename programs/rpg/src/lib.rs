9f088185be7b8bd43cd14d4abe95224d5143f3b7
openmymai <maicmi@me.com>
2024-11-02 15:10:51 +0700

    Solana Program Architect RPG Game

---
+++ programs/rpg/src/lib.rs
@@ -0,0 +1,51 @@
+ 
+// Insider src/lib.rs
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::log::sol_log_compute_units;
+ 
+mod state;
+mod instructions;
+mod constants;
+mod helpers;
+mod error;
+ 
+use state::*;
+use constants::*;
+use instructions::*;
+ 
+declare_id!("DgPnfVNWoxHBW4LC9a2VypLHtxHazTTvpw2M2SN5P7hw");
+ 
+#[program]
+pub mod rpg {
+    use super::*;
+ 
+    pub fn create_game(ctx: Context<CreateGame>, max_items_per_player: u8) -> Result<()> {
+        run_create_game(ctx, max_items_per_player)?;
+        sol_log_compute_units();
+        Ok(())
+    }
+ 
+    pub fn create_player(ctx: Context<CreatePlayer>) -> Result<()> {
+        run_create_player(ctx)?;
+        sol_log_compute_units();
+        Ok(())
+    }
+ 
+    pub fn spawn_monster(ctx: Context<SpawnMonster>) -> Result<()> {
+        run_spawn_monster(ctx)?;
+        sol_log_compute_units();
+        Ok(())
+    }
+ 
+    pub fn attack_monster(ctx: Context<AttackMonster>) -> Result<()> {
+        run_attack_monster(ctx)?;
+        sol_log_compute_units();
+        Ok(())
+    }
+ 
+    pub fn deposit_action_points(ctx: Context<CollectActionPoints>) -> Result<()> {
+        run_collect_action_points(ctx)?;
+        sol_log_compute_units();
+        Ok(())
+    }
+}
---
+++ programs/rpg/src/state/monster.rs
@@ -0,0 +1,8 @@
+use anchor_lang::prelude::*;
+#[account]
+#[derive(InitSpace)]
+pub struct Monster {
+    pub player: Pubkey,
+    pub game: Pubkey,
+    pub hitpoints: u64,
+}
